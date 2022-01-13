efe9c27d5755e94501d7a74217413b3096dab233
B4MB4N9-ID <68556996+B4MBANG-94NT3NG@users.noreply.github.com>
2022-01-13 18:02:04 +0700

    update api.js

--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,13 @@ const { mp4, Mp3 } = require('../lib/youtube');
 const { cekKey, checkLimit, resetLimit } = require('../database/db'); 
 const { youtubePlay, youtubeMp4, youtubeMp3, igdownloader, twitterdownloader } = require('../controllers/yt');
 const { cakLontong, bijak, quotes, fakta, ptl, motivasi, indonesia, malaysia, thailand, vietnam, korea, japan, naruto, china, tiktok, asupan, geayubi, ukhty, rikagusriani, anony, hijaber, joker, harley, cecan, santuy, bocil } = require('../controllers/randomtext');
+const hx = require('hxz-api');
+const fs = require('fs-extra');
+const util = require('minecraft-server-util');
+const options = {
+    timeout: 1000 * 5,
+    enableSRV: true // SRV record lookup
+};
 const { photoOxy } = require('../controllers/oxy');
 const { tgContr } = require('../controllers/tebakgambar');
 const { mDo } = require('../controllers/media');
@@ -23,6 +30,103 @@ router.get('/checkkey', async (req, res) => {
     });
     const limit = await checkLimit(apikey);
     res.send({status: 200, apikey: apikey, limit: limit});
+});
+
+router.get('/minecraft', async (req, res) => {
+    const apik = req.query.ipaddress;
+    const apikey = req.query.apikey;
+    if (apikey === undefined) return res.status(404).send({
+        status: 404,
+        message: `Input Parameter apikey`
+    });
+    const check = await cekKey(apikey);
+    if (!check) return res.status(403).send({
+        status: 403,
+        message: `apikey ${apikey} not found, please register first!`
+    });
+    util.status(apik, 25565, options)
+    .then((result) =>
+    res.json(result
+))
+});
+// akhir minecraft
+// lirik
+router.get('/lirik', async (req, res) => {
+    const judul = req.query.search;
+    const apikey = req.query.apikey;
+    if (apikey === undefined) return res.status(404).send({
+        status: 404,
+        message: `Input Parameter apikey`
+    });
+const check = await cekKey(apikey);
+    if (!check) return res.status(403).send({
+        status: 403,
+        message: `apikey ${apikey} not found, please register first!`
+    });
+
+hx.lirik(judul)
+    .then(result => {
+     res.json(result)
+})
+});
+// akhir lirik
+// igstalk
+router.get('/igstalk', async (req, res) => {
+    const username = req.query.username;
+    const apikey = req.query.apikey;
+    if (apikey === undefined) return res.status(404).send({
+        status: 404,
+        message: `Input Parameter apikey`
+    });
+const check = await cekKey(apikey);
+    if (!check) return res.status(403).send({
+        status: 403,
+        message: `apikey ${apikey} not found, please register first!`
+    });
+
+hx.igstalk(username)
+    .then(result => {
+    res.json(result)
+})
+});
+//akhir igstalk
+// pinterest
+router.get('/pinterest', async (req, res) => {
+    const judul = req.query.query;
+    const apikey = req.query.apikey;
+    if (apikey === undefined) return res.status(404).send({
+        status: 404,
+        message: `Input Parameter apikey`
+    });
+const check = await cekKey(apikey);
+    if (!check) return res.status(403).send({
+        status: 403,
+        message: `apikey ${apikey} not found, please register first!`
+    });
+
+    hx.pinterest(judul)
+    .then(result => {
+     res.json(result)
+})
+});
+// akhir pinterest
+// tiktok
+router.get('/tiktok', async (req, res) => {
+    const link = req.query.url;
+    const apikey = req.query.apikey;
+    if (apikey === undefined) return res.status(404).send({
+        status: 404,
+        message: `Input Parameter apikey`
+    });
+const check = await cekKey(apikey);
+    if (!check) return res.status(403).send({
+        status: 403,
+        message: `apikey ${apikey} not found, please register first!`
+    });
+    hx.ttdownloader(link)
+    .then(result => {
+    res.json(result)
+})
 });
 
 router.get('/tiktok', tIk);
