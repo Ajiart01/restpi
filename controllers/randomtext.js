9f772787528a83c6e2df1ac8a357edd3e4c46c84
B4MB4N9-ID <68556996+B4MBANG-94NT3NG@users.noreply.github.com>
2022-01-14 11:17:25 +0700

    update randomtext.js

--- controllers/randomtext.js
+++ controllers/randomtext.js
@@ -1008,28 +1008,6 @@ async function family100(req, res) {
     });
 }
 
-async function dare(req, res) {
-    const apikey = req.query.apikey;
-    if (apikey === undefined) return res.status(404).send({
-        status: 404,
-        message: `Input Parameter apikey`
-    });
-    const check = await cekKey(apikey);
-    if (!check) return res.status(403).send({
-        status: 403,
-        message: `apikey ${apikey} not found, please register first!`
-    });
-    let limit = await isLimit(apikey);
-    if (limit) return res.status(403).send({status: 403, message: 'your limit is 0, reset every morning'});
-    readFileTxt('./lib/data/dare.txt').then(result => {
-        limitAdd(apikey);
-        res.status(200).send({status: 200, result: result});
-    }).catch(error => {
-        console.log(error);
-        res.status(500).send({status: 500, message: 'Internal Server Error'});
-    });
-}
-
 async function truth(req, res) {
     const apikey = req.query.apikey;
     if (apikey === undefined) return res.status(404).send({
