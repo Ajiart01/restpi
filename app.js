21b8a5a0e2598b3268e308ef13637a16b94d3e03
B4MB4N9-ID <68556996+B4MBANG-94NT3NG@users.noreply.github.com>
2022-01-14 19:03:24 +0700

    update app.js

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
 const axios = require("axios")
 const cheerio = require("cheerio")
-const express = require('express')
-const secure = require('ssl-express-www')
-const morgan = require('morgan')
-const multer = require('multer')
-const path = require('path')
-const fs = require('fs')
-var app = express();
-app.use(secure)
-app.use(morgan('dev'));
-app.use(express.static('client'));
-app.set("json spaces",2)
-__path = process.cwd();
+const express = require('express');
+const app = express();
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const expressLayout = require('express-ejs-layouts');
@@ -24,22 +14,12 @@ const rateLimit = require("express-rate-limit");
 const apiRouters = require('./routes/api');
 const userRouters = require('./routes/users');
 const premiumRouters = require('./routes/premium');
+
 const { isAuthenticated } = require('./lib/auth');
 const { connectMongoDb } = require('./database/connect');
 const { getApikey, resetLimit } = require('./database/db');
 const { port } = require('./lib/settings');
 const { ignoreFavicon } = require('./lib/function');
-var downloader = require('./server/downloader.js');
-var search = require('./server/search.js');
-var anime = require('./server/anime.js');
-var randomimg = require('./server/randomimage.js');
-var nsfw = require('./server/nsfw.js');
-var photooxy = require('./server/photooxy.js');
-var otakudesu = require('./server/otakudesu.js');
-var games = require('./server/games.js');
-var primbon = require('./server/primbon.js');
-var convert = require('./server/convert.js');
-var other = require('./server/other.js');
 const { ExpiredTime, getTotalReq, getTodayReq, getVisitor, getTotalUser, addRequest, addVisitor } = require('./database/premium');
 
 const PORT = process.env.PORT || port;
@@ -130,20 +110,9 @@ app.get('/price', (req, res) => {
   })
 })
 
-app.use('/api', apiRouters)
-app.use('/users', userRouters)
-app.use('/premium', premiumRouters)
-app.use('/downloader', downloader)
-app.use('/search', search)
-app.use('/anime', anime)
-app.use('/randomimg', randomimg)
-app.use('/nsfw', nsfw)
-app.use('/photooxy', photooxy)
-app.use('/otakudesu', otakudesu)
-app.use('/games', games)
-app.use('/primbon', primbon)
-app.use('/converter', convert)
-app.use('/other', other)
+app.use('/api', apiRouters);
+app.use('/users', userRouters);
+app.use('/premium', premiumRouters);
 
 app.set('json spaces', 4);
 
