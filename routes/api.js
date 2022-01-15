b2bdef0a5209a7959d75607327c9644820439ecb
B4MB4N9-ID <68556996+B4MBANG-94NT3NG@users.noreply.github.com>
2022-01-15 19:55:55 +0700

    update api.js

--- routes/api.js
+++ routes/api.js
@@ -5,9 +5,7 @@ const fs = require('fs');
 const { getBuffer } = require('../lib/function');
 const { readFileTxt, readFileJson } = require('../lib/function');
 const { mp4, Mp3 } = require('../lib/youtube');
-const { merdekaNews } = require('../scraper/merdekanews');
 const { limitAdd, isLimit } = require('../database/db');
-const { artinama, ramalanJodoh } = require('../scraper/primbon');
 const { cekKey, checkLimit, resetLimit } = require('../database/db'); 
 const { youtubePlay, youtubeMp4, youtubeMp3, igdownloader, twitterdownloader } = require('../controllers/yt');
 const { cakLontong, bijak, quotes, fakta, ptl, motivasi, indonesia, malaysia, thailand, vietnam, korea, japan, naruto, china, tiktok, asupan, geayubi, ukhty, rikagusriani, anony, hijaber, joker, harley, cecan, santuy, bocil, tebakjenaka, tebaklirik, ppcouple, tebakchara, tebakbendera, tebakkabupaten, tebakkimia, tebakkata, tebakkalimat, susunkata, tekateki, dadu, asahotak, truth, dare, tebaktebakan, family100, storyanime, quotenime, loli, milf, husbu, aesthetic,  cosplay, shota, waifu, wallml, nekonime, ahegao, panties, gangbang, yuri, tentacles, zettairyouiki, thighs, sfwneko, pussy, nsfwneko, orgy, masturbation, manga, jahy, hentai, hentaigift, glasses, foot, femdom, cum, ero, cuckold, blowjob, ass, bdsm } = require('../controllers/randomtext');
@@ -39,84 +37,6 @@ router.get('/checkkey', async (req, res) => {
     const limit = await checkLimit(apikey);
     res.send({status: 200, apikey: apikey, limit: limit});
 });
-
-router.get('/merdeka', async(req, res) => {
-  const apikey = req.query.apikey;
-     if (apikey === undefined) return res.status(404).send({
-        status: 404,
-        message: `Input Parameter apikey`
-    });
-    const check = await cekKey(apikey);
-    if (!check) return res.status(403).send({
-        status: 403,
-        message: `apikey ${apikey} not found, please register first!`
-    });
-    let limit = await isLimit(apikey);
-    if (limit) return res.status(403).send({status: 403, message: 'your limit is 0, reset every morning'});
- const result = await merdekaNews();
- limitAdd(apikey);
- res.json({ result })
-})
-
-router.get("/qrcode", (req, res) => {
- const qr = require('qr-image');
- const text = req.query.text;
- const apikey = req.query.apikey;
-   if (text === undefined || apikey === undefined) return res.status(404).send({
-        status: 404,
-        message: `Input Parameter link & apikey`
-    });
-    const check = await cekKey(apikey);
-    if (!check) return res.status(403).send({
-        status: 403,
-        message: `apikey ${apikey} not found, please register first!`
-    });
-    let limit = await isLimit(apikey);
-    if (limit) return res.status(403).send({status: 403, message: 'your limit is 0, reset every morning'});
- const img = qr.image(text,{size :13});
- limitAdd(apikey);
- res.writeHead(200, {'Content-Type': 'image/png'});
- img.pipe(res);
-});
-
-/*
-router.get('/meme', async (req, res) => {
-     const apikey = req.query.apikey;
-        if (apikey === undefined) return res.status(404).send({
-        status: 404,
-        message: `Input Parameter link & apikey`
-    });
-    const check = await cekKey(apikey);
-    if (!check) return res.status(403).send({
-        status: 403,
-        message: `apikey ${apikey} not found, please register first!`
-    });
-    let limit = await isLimit(apikey);
-    if (limit) return res.status(403).send({status: 403, message: 'your limit is 0, reset every morning'});
-     const fetch = require('node-fetch');
-     const subReddits = ["dankmeme", "meme", "memes"];
-     const random = Math.floor(Math.random() * subReddits.length)
-     const body = await fetch('https://www.reddit.com/r/' + subReddits[random] + '/random/.json')
-     body = await body.json()
-     const a = body[0]
-     const title = a.data.children[0].data.title
-     const url = 'https://reddit.com'+a.data.children[0].data.permalink
-     const link = a.data.children[0].data.url_overridden_by_dest
-     const ups = a.data.children[0].data.ups
-     const comments = a.data.children[0].data.num_comments
-     const sub = a.data.children[0].data.subreddit_name_prefixed
-     const preview = a.data.children[0].data.preview
-     limitAdd(apikey);
-     return res.json({
-         status: true,
-         title: title, 
-         url: url, 
-         image: link, 
-         ups: ups, 
-         comments: comments 
-    });
- })
-*/
 
 router.get('/artinama', async(req, res) => {
 	const nama = req.query.nama;
