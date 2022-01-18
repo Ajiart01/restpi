e9bd844bff24c948ad273a5fc65353ac605e7290
B4MB4N9-ID <68556996+B4MBANG-94NT3NG@users.noreply.github.com>
2022-01-18 12:33:45 +0700

    update api.js

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,7 @@ const { limitAdd, isLimit } = require('../database/db');
 const { cekKey, checkLimit, resetLimit } = require('../database/db'); 
 const { cekKey } = require('../database/db'); 
 const { youtubePlay, youtubeMp4, youtubeMp3, igdownloader, twitterdownloader } = require('../controllers/yt');
-const { cakLontong, bijak, quotes, fakta, ptl, motivasi, indonesia, malaysia, thailand, vietnam, korea, japan, naruto, china, tiktok, asupan, geayubi, ukhty, rikagusriani, anony, hijaber, joker, harley, cecan, santuy, bocil, tebakjenaka, tebaklirik, ppcouple, tebakchara, tebakbendera, tebakkabupaten, tebakkimia, tebakkata, tebakkalimat, susunkata, tekateki, dadu, asahotak, truth, dare, tebaktebakan, family100, storyanime, quotenime, loli, milf, husbu, aesthetic,  cosplay, shota, waifu, wallml, nekonime, ahegao, panties, gangbang, yuri, tentacles, zettairyouiki, thighs, sfwneko, pussy, nsfwneko, orgy, masturbation, manga, jahy, hentai, hentaigift, glasses, foot, femdom, cum, ero, cuckold, blowjob, ass, bdsm } = require('../controllers/randomtext');
+const { cakLontong, bijak, quotes, fakta, ptl, motivasi, indonesia, malaysia, thailand, vietnam, korea, japan, naruto, china, tiktok, asupan, geayubi, ukhty, rikagusriani, anony, hijaber, joker, harley, cecan, santuy, bocil, tebakjenaka, tebaklirik, ppcouple } = require('../controllers/randomtext');
 
 
 router.get('/checkkey', async (req, res) => {
@@ -117,76 +117,6 @@ router.get('/game/susunkata', susunkata);
 
 router.get('/ppcouple', ppcouple);
 
-router.get('/anime/shota', shota);
 
-router.get('/anime/aesthetic', aesthetic);
-
-router.get('/anime/cosplay', cosplay);
-
-router.get('/anime/storyanime', storyanime);
-
-router.get('/anime/wallml', wallml);
-
-router.get('/anime/waifu', waifu);
-
-router.get('/anime/quotenime', quotenime);
-
-router.get('/anime/husbu', husbu);
-
-router.get('/anime/nekonime', nekonime);
-
-router.get('/anime/milf', milf);
-
-router.get('/anime/loli', loli);
-
-router.get('/nsfw/ahegao', ahegao);
-
-router.get('/nsfw/ass', ass);
-
-router.get('/nsfw/bdsm', bdsm);
-
-router.get('/nsfw/blowjob', blowjob);
-
-router.get('/nsfw/gangbang', gangbang);
-
-router.get('/nsfw/cum', cum);
-
-router.get('/nsfw/ero', ero);
-
-router.get('/nsfw/cuckold', cuckold);
-
-router.get('/nsfw/masturbation', masturbation);
-
-router.get('/nsfw/femdom', femdom);
-
-router.get('/nsfw/foot', foot);
-
-router.get('/nsfw/glasses', glasses);
-
-router.get('/nsfw/hentai', hentai);
-
-router.get('/nsfw/hentaigift', hentaigift);
-
-router.get('/nsfw/jahy', jahy);
-
-router.get('/nsfw/manga', manga);
-
-router.get('/nsfw/orgy', orgy);
-
-router.get('/nsfw/nsfwneko', nsfwneko);
-
-router.get('/nsfw/sfwneko', sfwneko);
-
-router.get('/nsfw/panties', panties);
-
-router.get('/nsfw/pussy', pussy);
-
-router.get('/nsfw/thighs', thighs);
-
-router.get('/nsfw/yuri', yuri);
-
-router.get('/nsfw/zettairyouiki', zettairyouiki);
-
-router.get('/nsfw/tentacles', tentacles);
 
 module.exports = router;
